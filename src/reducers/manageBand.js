1d302010b492ca3bfb60dbf570de484a47fc517d
Michelle Ranly <meranly8@gmail.com>
2021-06-16 02:03:46 -0500

    commit tree 99003055fc8bdbb64bd1ddf86220eb3d4f12164c

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,12 +1,19 @@
+import uuid from 'uuid'
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      const band = {
+        id: uuid(),
+        name: action.name
+      }
+      return { ...state, bands: [...state.bands, band] }
+    case 'DELETE_BAND':
+      console.log(`deleting ${action.id}`)
+      return {bands: state.bands.filter(band => band.id !== action.id)}
     default:
-      return state;
+      return state
   }
-};
+}
